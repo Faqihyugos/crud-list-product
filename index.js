dc0c5dce84802ba4084afec2b9e2a000a86a8c2f
Faqihyugos <faqihyugos@gmail.com>
2021-05-24 14:24:16 +0700

    foreach data di ejs

--- index.js
+++ index.js
@@ -4,13 +4,9 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-    res.render('hello.ejs');
-});
   
 // Tambahkan rute untuk halaman beranda
-app.get('/top',(req, res)=>{
+app.get('/',(req, res)=>{
   res.render('top.ejs')
 });
 
